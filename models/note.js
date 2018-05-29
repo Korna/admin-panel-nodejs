30b51a5afabd1c80fe2c3374bbd64d1d129c0f07
Korna <artem-koma@yandex.ru>
2018-05-29 13:03:45 +0300

    remove second constructor

--- models/note.js
+++ models/note.js
@@ -1,10 +1,5 @@
 module.exports = class Note{
 
-    constructor(text, title) {
-        this.text = text;
-        this.title = title;
-    }
-
     constructor(text, title, image) {
         this.text = text;
         this.title = title;
