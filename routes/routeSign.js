341a6085532b4a11361f0c82a7a715006d17bf5b
Korna <artem-koma@yandex.ru>
2018-05-31 21:31:52 +0300

    change custom callback for sign route

--- routes/routeSign.js
+++ routes/routeSign.js
@@ -3,13 +3,28 @@ const passport = require('passport');
 //const router = express.Router();
 
 module.exports = function(app, db) {
-    app.post('/api/signup', passport.authenticate('local-signup', function (req, res, next) {
-        res.status(200);//.end();
+    app.post('/api/signup', passport.authenticate('local-signup', function (req, res) {
+        res.status(200);
     }));
 
-    app.post('/api/signin', passport.authenticate('local-login', function (req, res, next) {
-            res.status(200);//.end();
-        }));
+
+    app.post('/api/signin', function (req, res, next) {
+        passport.authenticate('local', function (err, user, info) {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+
+              //  return res.redirect('/login');
+            }
+            req.logIn(user, function (err) {
+                if (err) {
+                    return next(err);
+                }
+                return res.redirect('/users/' + user.username);
+            });
+        })(req, res, next);
+    });
 
 };
 
